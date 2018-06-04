af5b1eb1371a4d3a6cd18ed8f511210e664ea74e
Lucid <29291906+IndecisiveBoolean@users.noreply.github.com>
2018-06-04 02:06:52 -0400

    remove random image selection from nosad command

--- commands/nosad.js
+++ commands/nosad.js
@@ -2,11 +2,7 @@ module.exports = {
     name: 'nosad',
     description: 'Please stop being sad...',
     execute(message, args) {
-       const images = ['https://i.imgur.com/oWq62Yy.png',
-                       'https://i.imgur.com/Qa7S6eP.png',
-                       'https://i.imgur.com/ItMih1m.gif']
         
-        const randomImage = images[Math.floor(Math.random() * images.length)];
-        message.channel.send({files: [randomImage]});
+        message.channel.send({files: ['https://i.imgur.com/ItMih1m.gif']});
     },
 };
